3cfe61b2a1ec6102f9a263f86f75efcc9680a255
lichanglong <1213035770@qq.com>
2018-09-06 15:34:59 +0800

    项目的创建

---
+++ dao/src/main/java/com/litan/dao/UsersDao.java
@@ -0,0 +1,8 @@
+package com.litan.dao;
+
+public class UsersDao {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111");
+        System.out.println("222222222222");
+    }
+}
